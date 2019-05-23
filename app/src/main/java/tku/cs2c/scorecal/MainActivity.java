56a96bae1ec40cdfa0589001420e387a3a98ef79
ppoppappc <ppoppappc@gmail.com>
2019-05-23 13:00:28 +0800

    Initial commit

---
+++ app/src/main/java/tku/cs2c/scorecal/MainActivity.java
@@ -0,0 +1,13 @@
+package tku.cs2c.scorecal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
