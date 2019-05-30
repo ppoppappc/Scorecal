10796d90ee7ef2d14b376a6c5a06032ca8ca914a
ppoppappc <ppoppappc@gmail.com>
2019-05-30 12:53:19 +0800

    second commit

--- app/src/main/java/tku/cs2c/scorecal/MainActivity.java
+++ app/src/main/java/tku/cs2c/scorecal/MainActivity.java
@@ -2,12 +2,37 @@ package tku.cs2c.scorecal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText etPrograming,etDataStruct,etAlgorithm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews(){
+        etPrograming = findViewById(R.id.editText);
+        etDataStruct = findViewById(R.id.editText2);
+        etAlgorithm = findViewById(R.id.editText3);
+    }
+    private boolean isValid(EditText editText){
+        String pattern = "1[0]{2}|[0-9]{1,2}";
+        String text = editText.getText().toString();
+
+        if(!text.matches(pattern)){
+            editText.setError("0~100");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public void onSubmitClick(View v){
+
     }
 }
